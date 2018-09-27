498221efb3891da0178a9981ab66c5b038dc4fb1
polar <2250649759@qq.com>
2018-09-27 14:41:42 +0800

    图表生成器增强鲁棒性

--- mineJs/diagram/TableFactory.js
+++ mineJs/diagram/TableFactory.js
@@ -5,6 +5,8 @@ define(["echarts", "BasicTools", "/polar/js/echarts/echarts-liquidfill.min.js"],
     function XPaneLoader(id, config) {
         const FHEIGHT = "height:100%;";
         const guid = tools.sGuid;
+        const sstd = (x) => !!x? x: "";
+        const nstd = (x) => !isNaN(parseFloat(x))? x: 0;
         (function () {
             let dom = document.createElement("div");
             __init__($(dom));
@@ -20,16 +22,16 @@ define(["echarts", "BasicTools", "/polar/js/echarts/echarts-liquidfill.min.js"],
                 // cols processing
                 function __col_owner__(i, row, cols) {
                     for (let j = 0; j < cols.length; j++) {
-                        let node = cols[j];
-                        if (node.column) {
-                            tbcnt.push(`<div class='${node.column}' style='${FHEIGHT};padding:0px;'>`);
+                        if (cols[j]) {
+                            let node = cols[j];
+                            tbcnt.push(`<div class='${sstd(node.column)}' style='${FHEIGHT};padding:0px;'>`);
                             switch (node.type) {
                                 case "title":
-                                    tbcnt.push(`<p class='${node.style}'>${node.name}</p>`);
+                                    tbcnt.push(`<p class='${sstd(node.style)}'>${sstd(node.name)}</p>`);
                                     break;
                                 case "chart":
                                     let _id = `ZXJ-${i + 1},${j + 1}-${guid()}`;
-                                    tbcnt.push(`<div id='${_id}' style='${FHEIGHT};'></div>`);
+                                    tbcnt.push(`<div id='${_id}' style='${FHEIGHT}; class='${sstd(node.class)}'></div>`);
                                     echDelay.push({
                                         id: _id,
                                         url: node.url
@@ -39,7 +41,7 @@ define(["echarts", "BasicTools", "/polar/js/echarts/echarts-liquidfill.min.js"],
                                     __row_owner__(node.rows);
                                     break;
                                 default:
-                                    tools.mutter(`unknown type:${node.type}`, "error");
+                                    tools.mutter(`unknown type:${node.type? node.type: "null"}`, "error");
                                     break;
                             }
                             tbcnt.push("</div>");
@@ -54,14 +56,14 @@ define(["echarts", "BasicTools", "/polar/js/echarts/echarts-liquidfill.min.js"],
                 // rows processing
                 function __row_owner__(rows) {
                     for (let i = 0; i < rows.length; i++) {
-                        let row = rows[i],
-                            height = parseInt(row.height);
-                        if (!isNaN(height)) {
-                            tbcnt.push(`<div class='row ${row.class}' style='height:${height}%;'>`);
+                        if (rows[i]) {
+                            let row = rows[i];
+                            tbcnt.push(`<div class='row ${sstd(row.class)}' style='height:${nstd(height)}%;'>`);
                             __col_owner__(i, row, row.cols);
                             tbcnt.push("</div>");
                         } else {
-                            tools.mutter(`row error: ${row.descrip? row.descrip: i + 1}`, "error");
+                            let descrip = row.descrip? row.descrip: i + 1;
+                            tools.mutter(`row error: ${descrip}`, "error");
                         }
                     }
                 }
@@ -69,23 +71,28 @@ define(["echarts", "BasicTools", "/polar/js/echarts/echarts-liquidfill.min.js"],
                 // pane processing
                 !function() {
                     try {
-                        if (config.pane) {
-                            for (let name in config.pane)
-                                jqDom.css(name, config.pane[name]);
+                        if (config) {
+                            if (config.pane) {
+                                for (let name in config.pane) {
+                                    jqDom.css(name, config.pane[name]);
+                                }
+                            } else {
+                                tools.mutter("pane not in config.", "error");
+                            }
+                            tbcnt.push(`<div class='container-fluid' style='${FHEIGHT}'>`);
+                            if (config.rows) {
+                                __row_owner__(config.rows);
+                            } else {
+                                tools.mutter("rows not in config.", "error");
+                            }
+                            tbcnt.push("</div>");
+                            jqDom.append(tbcnt.join('\n'));
+                            for (let i = 0; i < echDelay.length; ++i) {
+                                let node = echDelay[i];
+                                addChart(node.id, node.url);
+                            }
                         } else {
-                            console.error("pane not in config.");
-                        }
-                        tbcnt.push(`<div class='container-fluid' style='${FHEIGHT}'>`);
-                        if (config.rows) {
-                            __row_owner__(config.rows);
-                        } else {
-                            tools.mutter("rows not in config.", "error");
-                        }
-                        tbcnt.push("</div>");
-                        jqDom.append(tbcnt.join('\n'));
-                        for (let i = 0; i < echDelay.length; ++i) {
-                            let node = echDelay[i];
-                            addChart(node.id, node.url);
+                            tools.mutter("config is null", "error");
                         }
                     } catch(e) {
                         tools.mutter(e, "error");
